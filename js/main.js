f9b2e1cbd2a26aeb303b611c02ee3573427ae9c9
Amiri Hoare <amirihoare07@gmail.com>
2025-05-21 20:45:32 -0600

    added hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+function toggleMenu() {
+    const hamburgerIcon = document.querySelector('.hamburger__icon');
+    const menuLinks = document.querySelector('.menu-links');
+    
+    hamburgerIcon.classList.toggle('open');
+    menuLinks.classList.toggle('open');
+}
