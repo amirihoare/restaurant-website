581063346346c33b8d9b8683253bd7b2dbbbe60b
Amiri Hoare <amirihoare07@gmail.com>
2025-05-23 18:57:06 -0600

    applied styles to the specialty cuisine labels

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// window.addEventListener('load', function() {
+// 	setTimeout(() => {
+// 	  window.scrollTo(0, 0);
+// 	}, 0);
+// });
+
 function toggleMenu() {
     const hamburgerIcon = document.querySelector('.hamburger__icon');
     const menuLinks = document.querySelector('.menu-links');
