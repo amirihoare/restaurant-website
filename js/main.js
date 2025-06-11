c20c797096f8d08885c7e0c8596a9bd3c02d8600
Amiri Hoare <amirihoare07@gmail.com>
2025-06-10 19:26:17 -0600

    updated scroll behavior to enhance mobile user experience

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-window.addEventListener('load', function() {
-	setTimeout(() => {
-	  window.scrollTo(0, 0);
-	}, 0);
-});
+// reload scroll behavior
+window.addEventListener('load', function () {
+  const isReload =
+    performance.navigation.type === performance.navigation.TYPE_RELOAD ||
+    performance.getEntriesByType("navigation")[0]?.type === "reload"
+
+  const delay = isReload ? 100 : 0
+
+  setTimeout(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }, delay)
+})
 
 function toggleMenu() {
-    const hamburgerIcon = document.querySelector('.hamburger__icon');
-    const menuLinks = document.querySelector('.menu-links');
+    const hamburgerIcon = document.querySelector('.hamburger__icon')
+    const menuLinks = document.querySelector('.menu-links')
     
-    hamburgerIcon.classList.toggle('open');
-    menuLinks.classList.toggle('open');
+    hamburgerIcon.classList.toggle('open')
+    menuLinks.classList.toggle('open')
 }
